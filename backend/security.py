05e263c9a305ede3490da14b96cf098ac971b9b8
Alexey <agoodwill04@gmail.com>
2026-03-27 16:02:20 +0500

    настройки входа в админку

--- backend/security.py
+++ backend/security.py
@@ -9,7 +9,7 @@ def hash_password(password: str) -> str:
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
 def verify_password(password: str, password_hash: str) -> bool:
-    return bcrypt.checkpw(password.encode(), password_hash.encode())
+    return bcrypt.checkpw(password.encode(), password_hash.encode('utf-8', errors='ignore'))
 
 def create_access_token(data: dict):
     to_encode = data.copy()
