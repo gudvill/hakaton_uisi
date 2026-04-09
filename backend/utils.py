2328f3b35d1bb0eca6e501e38e79e6aa20414697
Alexey <agoodwill04@gmail.com>
2026-04-10 01:51:53 +0500

    настройка восстановления пароля

--- backend/utils.py
+++ backend/utils.py
@@ -3,7 +3,7 @@ from email.mime.text import MIMEText
 import os
 
 
-SMTP_SERVER = "smtp.yandex.ru"
+SMTP_SERVER = "77.88.21.158"
 SMTP_PORT = 465
 SMTP_LOGIN = "agoodwill04@gmail.com"
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
