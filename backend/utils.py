6c3ad2c32e10a2466f6c932da1299e81d2c21e0c
Alexey <agoodwill04@gmail.com>
2026-04-12 20:39:08 +0500

    настройка отправки писем

--- backend/utils.py
+++ backend/utils.py
@@ -3,23 +3,23 @@ from email.mime.text import MIMEText
 import os
 
 
-SMTP_SERVER = "108.177.14.109"
+SMTP_SERVER = "77.88.21.158"
 SMTP_PORT = 465
-SMTP_LOGIN = "agoodwill04@gmail.com"
-SMTP_PASSWORD = "iqbqqkwcswijxvma"
+SMTP_LOGIN = "alex.goodwill04@yandex.ru"
+SMTP_PASSWORD = "uhtetyxjrvuihgpo"
 
 def send_reset_email(to_email: str, reset_link: str):
     print("[EMAIL] Sending reset email...", flush=True)
     
     msg = MIMEText(f"Ссылка для сброса пароля:\n\n{reset_link}", "plain", "utf-8")
     msg["Subject"] = "Восстановление пароля"
-    msg["From"] = SMTP_LOGIN
+    msg["From"] = "alex.goodwill04@yandex.ru"  # Твой Gmail
     msg["To"] = to_email
     
     try:
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT, timeout=30) as server:
-            print(f"[EMAIL] Connected to {SMTP_SERVER}:{SMTP_PORT}", flush=True)
-            server.login(SMTP_LOGIN, SMTP_PASSWORD)
+        with smtplib.SMTP_SSL("77.88.21.158", 465, timeout=30) as server:
+            print("[EMAIL] SSL Connected to Yandex:465", flush=True)
+            server.login("alex.goodwill04@yandex.ru", "uhtetyxjrvuihgpo")  # Твои creds
             print("[EMAIL] Login OK", flush=True)
             server.send_message(msg)
             print("[EMAIL] Sent successfully!", flush=True)
