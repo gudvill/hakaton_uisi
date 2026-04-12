51323277c128adb90a219f9ab310cdd21cf4f892
Alexey <agoodwill04@gmail.com>
2026-04-12 21:10:47 +0500

    настройка отправки писем

--- backend/utils.py
+++ backend/utils.py
@@ -3,22 +3,28 @@ from email.mime.text import MIMEText
 import os
 
 
-SMTP_SERVER = "mailhog"
-SMTP_PORT = 1025
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 2525
+SMTP_LOGIN = "agoodwill04@gmail.com"
+SMTP_PASSWORD = "iqbq qkwc swij xvma"
 
 def send_reset_email(to_email: str, reset_link: str):
-    print("[EMAIL] Sending to MailHog...", flush=True)
-    
-    msg = MIMEText(f"Ссылка для сброса пароля:\n\n{reset_link}", "plain", "utf-8")
+    print("[EMAIL] Sending reset email...", flush=True)
+
+    msg = MIMEText(
+        f"Ссылка для сброса пароля:\n\n{reset_link}",
+        "plain",
+        "utf-8"
+    )
+
     msg["Subject"] = "Восстановление пароля"
-    msg["From"] = "admin@hakaton.local"
+    msg["From"] = SMTP_LOGIN
     msg["To"] = to_email
-    
-    try:
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT, timeout=10) as server:
-            print(f"[EMAIL] Connected to MailHog:{SMTP_PORT}", flush=True)
-            server.send_message(msg)  # Без login/starttls!
-            print("[EMAIL] Sent to MailHog OK!", flush=True)
-    except Exception as e:
-        print(f"[EMAIL] ERROR: {type(e).__name__}: {str(e)}", flush=True)
-        raise
+
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+        server.set_debuglevel(1)
+        server.starttls()
+        server.login(SMTP_LOGIN, SMTP_PASSWORD)
+        server.send_message(msg)
+
+    print("[EMAIL] Sent successfully", flush=True)
