1010b7ad2132ff6c2f5881e63969db3f774a7b2f
Alexey <agoodwill04@gmail.com>
2026-04-16 23:31:18 +0500

    настрока почты

--- backend/utils.py
+++ backend/utils.py
@@ -1,30 +1,47 @@
-import smtplib
-from email.mime.text import MIMEText
-import os
+import requests
 
- 
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 2525
-SMTP_LOGIN = "agoodwill04@gmail.com"
-SMTP_PASSWORD = "iqbq qkwc swij xvma"
+CLIENT_ID = "sp_id_ea20d23ab97db44504db0bc1ffa8577d"
+CLIENT_SECRET = "sp_apikey_e5107e56964ff5b290cf36a64bdb78a655953a5443d42feb9314b56b2407a025"
+
+def get_token():
+    url = "https://api.sendpulse.com/oauth/access_token"
+    data = {
+        "grant_type": "client_credentials",
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+    }
+
+    response = requests.post(url, data=data)
+    return response.json()["access_token"]
+
 
 def send_reset_email(to_email: str, reset_link: str):
-    print("[EMAIL] Sending reset email...", flush=True)
+    token = get_token()
 
-    msg = MIMEText(
-        f"Ссылка для сброса пароля:\n\n{reset_link}",
-        "plain",
-        "utf-8"
-    )
+    url = "https://api.sendpulse.com/smtp/emails"
 
-    msg["Subject"] = "Восстановление пароля"
-    msg["From"] = SMTP_LOGIN
-    msg["To"] = to_email
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+    }
 
-    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-        server.set_debuglevel(1)
-        server.starttls()
-        server.login(SMTP_LOGIN, SMTP_PASSWORD)
-        server.send_message(msg)
+    data = {
+        "email": {
+            "html": f"<p>Ссылка для сброса пароля:</p><a href='{reset_link}'>{reset_link}</a>",
+            "text": f"Ссылка для сброса пароля: {reset_link}",
+            "subject": "Восстановление пароля",
+            "from": {
+                "name": "Hakaton",
+                "email": "agoodwill04@hakaton1.bizml.ru"
+            },
+            "to": [
+                {
+                    "email": to_email
+                }
+            ]
+        }
+    }
 
-    print("[EMAIL] Sent successfully", flush=True)
+    response = requests.post(url, json=data, headers=headers)
+
+    print(response.status_code, response.text)
