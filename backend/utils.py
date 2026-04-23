2fc2e2c5ddcc247fca4dde0e356b97742739d982
Alexey <agoodwill04@gmail.com>
2026-04-23 20:42:02 +0500

    очередная попытка отправить письмо восстановления пароля

--- backend/utils.py
+++ backend/utils.py
@@ -1,54 +1,14 @@
 import requests
 import time
 
-CLIENT_ID = "cb2131530fc86aa3a81b1a9b3c27e766"
-CLIENT_SECRET = "76f40dc7f26caf1b83044463dab03db3"
-
-_token_cache = {
-    "access_token": None,
-    "expires_at": 0
-}
-
-
-def get_token():
-    if _token_cache["access_token"] and time.time() < _token_cache["expires_at"]:
-        return _token_cache["access_token"]
-
-    url = "https://api.sendpulse.com/oauth/token"
-
-    data = {
-        "grant_type": "client_credentials",
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-    }
-
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-
-    response = requests.post(url, data=data, headers=headers)
-    print("CLIENT_ID:", CLIENT_ID)
-    print("CLIENT_SECRET:", CLIENT_SECRET)
-    print(response.status_code, response.text)
-
-    data = response.json()
-
-    if "access_token" not in data:
-        raise Exception(f"Auth failed: {data}")
-
-    _token_cache["access_token"] = data["access_token"]
-    _token_cache["expires_at"] = time.time() + 3500
-
-    return data["access_token"]
-
 
 def send_reset_email(to_email: str, reset_link: str):
-    token = get_token()
+    API_KEY = "sp_apikey_e5107e56964ff5b290cf36a64bdb78a655953a5443d42feb9314b56b2407a025"
 
     url = "https://api.sendpulse.com/smtp/emails"
 
     headers = {
-        "Authorization": f"Bearer {token}",
+        "Authorization": f"Bearer {API_KEY}",
         "Content-Type": "application/json",
     }
 
@@ -69,12 +29,9 @@ def send_reset_email(to_email: str, reset_link: str):
 
     response = requests.post(url, json=payload, headers=headers)
 
-    print("[SENDPULSE] email status:", response.status_code)
-    print("[SENDPULSE] email response:", response.text)
+    print(response.status_code, response.text)
 
     data = response.json()
 
     if not data.get("result"):
         raise Exception(f"SendPulse error: {data}")
-
-    print("[SENDPULSE] email sent successfully")
