f582862ed724a8d8391c4a28a3cc237dbc1faa17
Alexey <agoodwill04@gmail.com>
2026-04-23 22:30:48 +0500

    очередная попытка отправить письмо восстановления пароля

--- backend/utils.py
+++ backend/utils.py
@@ -1,37 +1,17 @@
-import requests
-import time
+import resend
 
+resend.api_key = "re_Q5xonnYt_3y4dacoYGzQmdT6SBN86HNjG"
 
 def send_reset_email(to_email: str, reset_link: str):
-    API_KEY = "sp_apikey_e5107e56964ff5b290cf36a64bdb78a655953a5443d42feb9314b56b2407a025"
+    response = resend.Emails.send({
+        "from": "Hakaton <onboarding@resend.dev>",
+        "to": [to_email],
+        "subject": "Восстановление пароля",
+        "html": f"""
+            <p>Ссылка для сброса пароля:</p>
+            <a href="{reset_link}">{reset_link}</a>
+        """
+    })
 
-    url = "https://api.sendpulse.com/smtp/emails"
-
-    headers = {
-        "Authorization": f"Bearer {API_KEY}",
-        "Content-Type": "application/json",
-    }
-
-    payload = {
-        "email": {
-            "html": f"<p>Ссылка для сброса пароля:</p><a href='{reset_link}'>{reset_link}</a>",
-            "text": f"Ссылка для сброса пароля: {reset_link}",
-            "subject": "Восстановление пароля",
-            "from": {
-                "name": "Hakaton",
-                "email": "noreply@sendpulse.com"
-            },
-            "to": [
-                {"email": to_email}
-            ]
-        }
-    }
-
-    response = requests.post(url, json=payload, headers=headers)
-
-    print(response.status_code, response.text)
-
-    data = response.json()
-
-    if not data.get("result"):
-        raise Exception(f"SendPulse error: {data}")
+    print("[RESEND] response:", response)
+    return response
