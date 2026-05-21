57b10cba2f8bc9548dcf14a2a6f27536901ac707
Alexey <agoodwill04@gmail.com>
2026-05-21 18:08:57 +0500

    токены в куках

--- frontend/src/routes/AdminRoute.js
+++ frontend/src/routes/AdminRoute.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { getMe } from "../api/authService";
 
 export default function AdminRoute({ children }) {
-  const token = localStorage.getItem("access");
-  if (!token) {
+  const [loading, setLoading] = useState(true);
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    getMe()
+      .then(() => {
+        setIsAuth(true);
+      })
+      .catch(() => {
+        setIsAuth(false);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Загрузка...</div>;
+  }
+
+  if (!isAuth) {
     return <Navigate to="/admin/login" />;
   }
+
   return children;
 }
